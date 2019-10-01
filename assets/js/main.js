1de6ebbebc59f8312f1435e2a726e6e3c195c051
danielkdewar <mail@daniel-dewar.com>
2019-10-01 12:19:41 -0500

    set up form and styling

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+(function($) {
+
+    var form = $("#signup-form");
+    form.steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        labels: {
+            previous : 'Prev',
+            next : 'Next',
+            finish : 'Finish',
+            current : ''
+        },
+        titleTemplate : '<h3 class="title">#title#</h3>',
+        onFinished: function (event, currentIndex)
+        {
+            alert('Submitted');
+        }
+    });
+
+
+})(jQuery);
